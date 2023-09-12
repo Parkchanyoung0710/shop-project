dbd2d007d74d041ff12371ad93865f71cf1dad6c
park <call4892@gmail.com>
2023-09-12 21:27:33 +0900

    이미지, 메뉴바 수정하기

--- src/components/Product/Product.data.js
+++ src/components/Product/Product.data.js
@@ -37,9 +37,17 @@ export const bestData = [
     price: '59,000원',
     qty: 1,
   },
-
   {
     id: 2,
+    title: '세미오버 차이나 셔츠',
+    desc: '코디하기 좋은 베이직!',
+    image: 'best7',
+    price: '38,000원',
+    qty: 1,
+  },
+
+  {
+    id: 3,
     title: '캐주얼 파란 후드티',
     desc: '편하게 입는 후드티!',
     image: 'best2',
@@ -48,7 +56,7 @@ export const bestData = [
   },
 
   {
-    id: 3,
+    id: 4,
     title: '코튼밴딩 와이드팬츠',
     desc: '데일리룩으로 입는 팬츠!!',
     image: 'best3',
@@ -57,7 +65,7 @@ export const bestData = [
   },
 
   {
-    id: 4,
+    id: 5,
     title: '시그니처 무지셔츠',
     desc: '시원한 오버셔츠입니다!',
     image: 'best4',
@@ -66,11 +74,35 @@ export const bestData = [
   },
 
   {
-    id: 5,
+    id: 6,
     title: '그레이스 와이드데님',
     desc: '시원한 와이드팬츠!',
     image: 'best5',
     price: '50,000원',
+    qty: 1,
+  },
+  {
+    id: 7,
+    title: '오버핏 피그 셔츠',
+    desc: '깔끔한 핏의 셔츠!',
+    image: 'best6',
+    price: '100,000원',
+    qty: 1,
+  },
+  {
+    id: 8,
+    title: '베이직 트렌치코트',
+    desc: '핏이 좋은 트렌치코트!',
+    image: 'best8',
+    price: '69,000원',
+    qty: 1,
+  },
+  {
+    id: 9,
+    title: '베이직 세미오버 셔츠',
+    desc: '베이직 셔츠!',
+    image: 'best9',
+    price: '49,000원',
     qty: 1,
   },
 ];
@@ -79,7 +111,7 @@ export const bestData = [
 
 export const TopData = [
   {
-    id: 3,
+    id: 1,
     title: '머슬핏 아이스 반팔',
     desc: '헨릭넥, 라운드넥!',
     price: '59,000원',
@@ -88,7 +120,7 @@ export const TopData = [
   },
 
   {
-    id: 5,
+    id: 2,
     title: '유니버시티 반팔티',
     desc: '전면 프린팅 포인트!',
     price: '20,000원',
@@ -97,7 +129,7 @@ export const TopData = [
   },
 
   {
-    id: 6,
+    id: 3,
     title: '7부 슬라브 박스티',
     desc: '루즈한 핏인 박스티!',
     price: '26,000원',
@@ -106,7 +138,7 @@ export const TopData = [
   },
 
   {
-    id: 7,
+    id: 4,
     title: '실키 U넥 반팔',
     desc: '시원한 유넥 반팔티!',
     price: '16,000원',
@@ -115,7 +147,7 @@ export const TopData = [
   },
 
   {
-    id: 9,
+    id: 5,
     title: '클래식단가라 반팔',
     desc: '쾌적한 착용감!',
     price: '35,000원',
@@ -124,11 +156,35 @@ export const TopData = [
   },
 
   {
-    id: 10,
+    id: 6,
     title: '에이드분또 반팔',
     desc: '핏이 살아있는 티!',
     price: '22,000원',
     image: 'top6',
+    qty: 1,
+  },
+  {
+    id: 7,
+    title: '이중지 후드티',
+    desc: '심플한 티!',
+    price: '43,000원',
+    image: 'top11',
+    qty: 1,
+  },
+  {
+    id: 8,
+    title: '오버핏 에어 긴팔',
+    desc: '쿨링 티셔츠 긴팔!',
+    price: '25,000원',
+    image: 'top12',
+    qty: 1,
+  },
+  {
+    id: 9,
+    title: '룩 플리츠 티',
+    desc: '스타일리쉬한 티!',
+    price: '39,000원',
+    image: 'top13',
     qty: 1,
   },
 ];
@@ -217,14 +273,14 @@ export const shirtData = [
     qty: 1,
   },
 
-  {
-    id: 10,
-    title: '데님 오버셔츠',
-    desc: '체형을 보완하는 셔츠!',
-    price: '36,000원',
-    image: 'sh9',
-    qty: 1,
-  },
+  // {
+  //   id: 10,
+  //   title: '데님 오버셔츠',
+  //   desc: '체형을 보완하는 셔츠!',
+  //   price: '36,000원',
+  //   image: 'sh9',
+  //   qty: 1,
+  // },
 ];
 
 export const PantsData = [
@@ -246,7 +302,7 @@ export const PantsData = [
     qty: 1,
   },
   {
-    id: 4,
+    id: 3,
     title: '카고 블랙 조거팬츠',
     desc: '만족하는 핏감!',
     price: '41,000원',
@@ -255,7 +311,7 @@ export const PantsData = [
   },
 
   {
-    id: 6,
+    id: 4,
     title: '오즈 슬림 데님팬츠',
     desc: '깔끔한 핏!',
     price: '108,000원',
@@ -264,7 +320,7 @@ export const PantsData = [
   },
 
   {
-    id: 7,
+    id: 5,
     title: '리얼 아이스 팬츠',
     desc: '편안한 와이드 팬츠!',
     price: '20,000원',
@@ -273,7 +329,7 @@ export const PantsData = [
   },
 
   {
-    id: 8,
+    id: 6,
     title: '모즐리 반바지',
     desc: '부드럽고 편안한 착용감!',
     price: '29,900원',
@@ -282,7 +338,7 @@ export const PantsData = [
   },
 
   {
-    id: 9,
+    id: 7,
     title: '세미와이드 카고팬츠',
     desc: '사이드 카고 포켓 팬츠!',
     price: '44,500원',
@@ -291,11 +347,19 @@ export const PantsData = [
   },
 
   {
-    id: 10,
+    id: 8,
     title: '퍼스트 카고 반바지',
     desc: '여름에 시원한 반바지!',
     price: '27,000원',
     image: 'bt7',
+    qty: 1,
+  },
+  {
+    id: 9,
+    title: '룩 데이프 밴딩슬랙스',
+    desc: '베스트핏 슬랙스 바지!',
+    price: '27,000원',
+    image: 'bt10',
     qty: 1,
   },
 ];
@@ -307,7 +371,7 @@ export const WeatherData = [
     desc: '시원한 여름 코디!',
     price: '30,000원',
     image: 'sh5',
-    temper: 32,
+    temper: 31,
     qty: 1,
   },
   {
@@ -316,7 +380,7 @@ export const WeatherData = [
     desc: '밑단 트임 포인트!',
     price: '38,000원',
     image: 'bt1',
-    temper: 32,
+    temper: 31,
     qty: 1,
   },
   {
@@ -325,7 +389,7 @@ export const WeatherData = [
     desc: '따뜻한 핏!',
     price: '35,000원',
     image: 'top7',
-    temper: 16,
+    temper: 17,
     qty: 1,
   },
   {
@@ -334,7 +398,7 @@ export const WeatherData = [
     desc: '깔끔한 핏!',
     price: '108,000원',
     image: 'bt3',
-    temper: 16,
+    temper: 17,
     qty: 1,
   },
   {
@@ -343,7 +407,7 @@ export const WeatherData = [
     desc: '퀄리티 높은 셔츠!',
     price: '37,000원',
     image: 'sh7',
-    temper: 20,
+    temper: 23,
     qty: 1,
   },
   {
@@ -352,7 +416,7 @@ export const WeatherData = [
     desc: '만족하는 핏감!',
     price: '41,000원',
     image: 'bt2',
-    temper: 20,
+    temper: 23,
   },
   {
     id: 7,
